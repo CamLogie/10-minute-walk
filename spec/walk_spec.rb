ccd7a6038d4d71fee8e1aacac62cd16b9cdb5312
Cameron Logie <cameronlogie24@gmail.com>
2020-12-01 17:52:30 +0000

    method ensures that walk is 10 minutes long

--- spec/walk_spec.rb
+++ spec/walk_spec.rb
@@ -5,4 +5,10 @@ describe "ten_minute_walk?" do
   it "Returns false when given ['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']" do
     expect(ten_minute_walk?(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to be false
   end
+  it "returns false when given ['w', 's']" do
+    expect(ten_minute_walk?(['w', 's'])).to be false
+  end
+  it "returns false when given ['s', 'n']" do
+    expect(ten_minute_walk?(["s", "n"])).to be false 
+  end
 end
--- walk.rb
+++ walk.rb
@@ -1,17 +1,16 @@
 def ten_minute_walk?(coordinates)
+  return false if coordinates.length != 10
+  
   n_s = 0
   e_w = 0
 
   coordinates.each do |coordinate|
-    if coordinate == "n"
-      n_s += 1
-    elsif coordinate == "s"
-      n_s -= 1
-    elsif coordinate == "e"
-      e_w += 1
-    else e_w -= 1
-    end
+    n_s += 1 if coordinate == "n"
+    n_s -= 1 if coordinate == "s"
+    e_w += 1 if coordinate == "e"
+    e_w -= 1 if coordinate == "w"
   end
   return true if n_s == 0 && e_w == 0
   false
+  
 end
